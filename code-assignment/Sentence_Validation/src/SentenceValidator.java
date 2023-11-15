8bbe3ae082f913cc9f0461561d0ac7f591d1dfe9
Declan <mccabed7@tcd.ie>
2023-11-15 18:17:32 +0000

    partially completed SentenceValidator

--- code-assignment/Sentence_Validation/src/SentenceValidator.java
+++ code-assignment/Sentence_Validation/src/SentenceValidator.java
@@ -13,9 +13,34 @@ public class SentenceValidator {
     public boolean validate(String sentence){
         if(sentence.length() < 2) return false;  //to ensure sentences have one letter, and one punctuation mark
 
+        //ensure capital letter or number for first char
         int firstCharCode = (int)sentence.charAt(0);
-        if((firstCharCode < 65 || firstCharCode > 90)  &&) return false;  //to ensure capital letter, or number
+        if((firstCharCode < 65 || firstCharCode > 90)  && (firstCharCode < 48 || firstCharCode > 57)) return false;
 
+        //ensure correct sentence termination
+        char lastChar = sentence.charAt(sentence.length() - 1);
+        if(lastChar != '.' && lastChar != '?' && lastChar != '!') return false;
+
+        int quotesCount = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentChar = sentence.charAt(i);
+
+            //ensure no more than 1 '.' char
+            if(currentChar == '.' && i != sentence.length() -1) return false;
+
+            if(currentChar == '"') quotesCount++;
+
+        }
+
+        //ensure even amount of quotes
+        if(quotesCount % 2 == 1) return false;
         return true;
+    }
+
+
+    //main method for convenient testing purposes
+    public static void main(String[] args) {
+        SentenceValidator validator = new SentenceValidator();
+        System.out.println(validator.validate("BAA"));
     }
 }
