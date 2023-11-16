b9f5e9c1c1436a2e1394aed1d924332dac3c2dda
Declan <mccabed7@tcd.ie>
2023-11-16 15:37:23 +0000

    Completed solution + tests

--- code-assignment/Sentence_Validation/src/SentenceValidator.java
+++ code-assignment/Sentence_Validation/src/SentenceValidator.java
@@ -1,19 +1,42 @@
 public class SentenceValidator {
 
+    private int countChar(String word, char c) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == c) count++;
+        }
+        return count;
+    }
 
-    // ----------------------------------------------------------
+    private boolean isValidNumber(String num){
+        if(num.length() == 1){
+            return false;
+        }
+        else{
+            if(num.charAt(0) == '1') {
+                switch (num.charAt(1)){
+                    case '1':
+                    case '2':
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
     /**
      * Determines whether a sentence is valid according to assignment rules
      *
      * @param sentence: The sentence to be validated
      *
-     * @return true if the sentence is valid, false otherwise
+     * @return True if the sentence is valid, false otherwise
      *
      */
     public boolean validate(String sentence){
         if(sentence.length() < 2) return false;  //to ensure sentences have one letter, and one punctuation mark
 
-        //ensure capital letter or number for first char
+        //ensure capital letter or number for first char (using ASCII codes for efficiency)
         int firstCharCode = (int)sentence.charAt(0);
         if((firstCharCode < 65 || firstCharCode > 90)  && (firstCharCode < 48 || firstCharCode > 57)) return false;
 
@@ -22,13 +45,22 @@ public class SentenceValidator {
         if(lastChar != '.' && lastChar != '?' && lastChar != '!') return false;
 
         int quotesCount = 0;
-        for (int i = 0; i < sentence.length(); i++) {
-            char currentChar = sentence.charAt(i);
+        String words[] = sentence.split(" ");  //java method, O(n) time, n = string length
+        for (int i = 0; i < words.length; i++) {
+                ;
+                //ensure no more than 1 '.' char
+                int stopCount = countChar(words[i], '.');
+                if((i == words.length -1 && stopCount != 1) || (i != words.length -1 && stopCount > 0)) return false;
 
-            //ensure no more than 1 '.' char
-            if(currentChar == '.' && i != sentence.length() -1) return false;
+                //count quotes
+                quotesCount += countChar(words[i], '"');
 
-            if(currentChar == '"') quotesCount++;
+                //to handle numbers
+                String wordCopy = words[i];
+                wordCopy = wordCopy.replace("\"", "");
+                wordCopy = wordCopy.replace(",", "");
+                wordCopy = wordCopy.replace(".", "");
+                if(wordCopy.length() >= 1 && (wordCopy.charAt(0) >= 48 || wordCopy.charAt(0) <= 57) && !isValidNumber(wordCopy)) return false;
 
         }
 
@@ -41,6 +73,7 @@ public class SentenceValidator {
     //main method for convenient testing purposes
     public static void main(String[] args) {
         SentenceValidator validator = new SentenceValidator();
-        System.out.println(validator.validate("BAA"));
+        System.out.println(validator.validate("Hello my name is \"Declan\", hi!, I'm 21."));
+        System.out.println(validator.validate("Hello my name is \"Declan\", hi!, I'm \"21\"."));
     }
 }
--- code-assignment/Sentence_Validation/src/SentenceValidatorTest.java
+++ code-assignment/Sentence_Validation/src/SentenceValidatorTest.java
@@ -1,2 +1,66 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SentenceValidatorTest {
+
+    @Test
+    public void validTest1() {
+        SentenceValidator validator = new SentenceValidator();
+        assertTrue("Testing valid: The quick brown fox said “hello Mr lazy dog”.", validator.validate("The quick brown fox said \"hello Mr lazy dog\"."));
+    }
+
+    @Test
+    public void validTest2() {
+        SentenceValidator validator = new SentenceValidator();
+        assertTrue("Testing valid: The quick brown fox said hello Mr lazy dog.", validator.validate("The quick brown fox said hello Mr lazy dog."));
+    }
+
+    @Test
+    public void validTest3() {
+        SentenceValidator validator = new SentenceValidator();
+        assertTrue("Testing valid: One lazy dog is too few, 13 is too many.", validator.validate("One lazy dog is too few, 13 is too many."));
+    }
+
+    @Test
+    public void validTest4() {
+        SentenceValidator validator = new SentenceValidator();
+        assertTrue("Testing valid: One lazy dog is too few, thirteen is too many.", validator.validate("One lazy dog is too few, thirteen is too many."));
+    }
+
+    @Test
+    public void invalidTest1FullStop(){
+        SentenceValidator validator = new SentenceValidator();
+        assertFalse("Testing invalid: The quick brown fox said \"hello Mr. lazy dog\".", validator.validate("The quick brown fox said \"hello Mr. lazy dog\"."));
+    }
+
+    @Test
+    public void invalidTest2CapitalLetter(){
+        SentenceValidator validator = new SentenceValidator();
+        assertFalse("Testing invalid: the quick brown fox said \"hello Mr lazy dog\".", validator.validate("the quick brown fox said \"hello Mr lazy dog\"."));
+    }
+
+    @Test
+    public void invalidTest3Quote(){
+        SentenceValidator validator = new SentenceValidator();
+        assertFalse("Testing invalid: \"The quick brown fox said “hello Mr lazy dog.\"", validator.validate("\"The quick brown fox said “hello Mr lazy dog.\""));
+    }
+
+    @Test
+    public void invalidTest4Number(){
+        SentenceValidator validator = new SentenceValidator();
+        assertFalse("Testing invalid: One lazy dog is too few, 12 is too many.", validator.validate("One lazy dog is too few, 12 is too many."));
+    }
+
+    @Test
+    public void invalidTest5Number(){
+        SentenceValidator validator = new SentenceValidator();
+        assertFalse("Testing invalid: Are there 11, 12, or 13 lazy dogs?", validator.validate("Are there 11, 12, or 13 lazy dogs?"));
+    }
+    @Test
+    public void invalidTest6FullStop(){
+        SentenceValidator validator = new SentenceValidator();
+        assertFalse("Testing invalid: There is no punctuation in this sentence", validator.validate("There is no punctuation in this sentence"));
+    }
+
 }
